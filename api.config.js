dad31922b98b0cc19a7fc3ac56a1330403a3004f
Jatinder Singh <1294763+jsbimra@users.noreply.github.com>
2020-06-15 22:26:38 +0530

    1xx.xx.xxx.xxx updated

--- api.config.js
+++ api.config.js
@@ -5,18 +5,18 @@ const apiVersion = 'v1';
 
 const hostname = window && window.location && window.location.hostname;
 
-// http://172.30.252.56:3001/services/validate-sid-ktp <<< dev
-// http://172.30.252.57:3001/services/validate-sid-ktp <<< staging and prod
+// http://1xx.xx.xxx.xxx:3001/services/validate-sid-ktp <<< dev
+// http://1xx.xx.xxx.xxx:3001/services/validate-sid-ktp <<< staging and prod
 
-//FE deployed PROD IP:  172.30.251.140
-//FE deployed DEV IP:  172.30.252.56
+//FE deployed PROD IP:  1xx.xx.xxx.xxx
+//FE deployed DEV IP:  1xxxxx
 
-if(hostname === '172.30.251.140') { //for production
-  backendHost = 'http://172.30.252.57:3001'; 
-} else if(hostname === '172.30.251.140') { //for staging condition
-  backendHost = 'http://172.30.252.57:3001'; 
-} else if(hostname === '172.30.252.56') { //for dev server 
-    backendHost = 'http://172.30.252.56:3001'; 
+if(hostname === '1xx.xx.xxx.xxx') { //for production
+  backendHost = 'http://1xx.xx.xxx.xxx:3001'; 
+} else if(hostname === '1xx.xx.xxx.xxx.140') { //for staging condition
+  backendHost = 'http://11xx.xx.xxx.xxx:3001'; 
+} else if(hostname === '1xx.xx.xxx.xxx') { //for dev server 
+    backendHost = 'http://1xx.xx.xxx.xxx:3001'; 
 } else if(/^qa/.test(hostname)) { //for QA
   backendHost = `https://api.${hostname}`;
 } else { //for localhost
